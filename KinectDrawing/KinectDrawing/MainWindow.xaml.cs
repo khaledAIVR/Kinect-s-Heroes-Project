ff7897ccb5ee048fc006aa5431703eba388b8dbc
khaledAIVR <48943763+khaledAIVR@users.noreply.github.com>
2020-10-22 07:31:26 +0200

    Adding project's folders to the repo

---
+++ KinectDrawing/KinectDrawing/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KinectDrawing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensor _sensor = null;
+        private ColorFrameReader _colorReader = null;
+        private BodyFrameReader _bodyReader = null;
+        private IList<Body> _bodies = null;
+
+        private int _width = 0;
+        private int _height = 0;
+        private byte[] _pixels = null;
+        private WriteableBitmap _bitmap = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _sensor = KinectSensor.GetDefault();
+
+            if (_sensor != null)
+            {
+                _sensor.Open();
+
+                _width = _sensor.ColorFrameSource.FrameDescription.Width;
+                _height = _sensor.ColorFrameSource.FrameDescription.Height;
+
+                _colorReader = _sensor.ColorFrameSource.OpenReader();
+                _colorReader.FrameArrived += ColorReader_FrameArrived;
+
+                _bodyReader = _sensor.BodyFrameSource.OpenReader();
+                _bodyReader.FrameArrived += BodyReader_FrameArrived;
+
+                _pixels = new byte[_width * _height * 4];
+                _bitmap = new WriteableBitmap(_width, _height, 96.0, 96.0, PixelFormats.Bgra32, null);
+
+                _bodies = new Body[_sensor.BodyFrameSource.BodyCount];
+
+                camera.Source = _bitmap;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_colorReader != null)
+            {
+                _colorReader.Dispose();
+            }
+
+            if (_bodyReader != null)
+            {
+                _bodyReader.Dispose();
+            }
+
+            if (_sensor != null)
+            {
+                _sensor.Close();
+            }
+        }
+
+        private void ColorReader_FrameArrived(object sender, ColorFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    frame.CopyConvertedFrameDataToArray(_pixels, ColorImageFormat.Bgra);
+
+                    _bitmap.Lock();
+                    Marshal.Copy(_pixels, 0, _bitmap.BackBuffer, _pixels.Length);
+                    _bitmap.AddDirtyRect(new Int32Rect(0, 0, _width, _height));
+                    _bitmap.Unlock();
+                }
+            }
+        }
+
+        private void BodyReader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    frame.GetAndRefreshBodyData(_bodies);
+
+                    Body body = _bodies.Where(b => b.IsTracked).FirstOrDefault();
+
+                    if (body != null)
+                    {
+                        Joint handRight = body.Joints[JointType.HandRight];
+                        Joint handLeft = body.Joints[JointType.HandLeft];
+
+                        if (body.HandRightState == HandState.Closed || body.HandRightState == HandState.Open)
+                        {
+
+                            
+                            CameraSpacePoint handRightPosition = handRight.Position;
+                            ColorSpacePoint handRightPoint = _sensor.CoordinateMapper.MapCameraPointToColorSpace(handRightPosition);
+
+                            float x = handRightPoint.X;
+                            float y = handRightPoint.Y;
+
+                            if (!float.IsInfinity(x) && ! float.IsInfinity(y))
+                            {
+
+                                // DRAW!
+                                if (body.HandRightState == HandState.Closed)
+                                    trail.Points.Add(new Point { X = x, Y = y });
+
+                                if (body.HandLeftState == HandState.Open)
+                                    if (trail.Stroke == Brushes.Red)
+                                        trail.Stroke = Brushes.Green;
+                                    else if (trail.Stroke == Brushes.Green)
+                                        trail.Stroke = Brushes.Blue;
+                                    else
+                                        trail.Stroke = Brushes.Red;
+
+                                if (body.HandLeftState == HandState.Lasso)
+                                {
+                                    canvas.Children.Remove(trail);
+                                    Polyline trail2 = new Polyline();
+                                    trail2.Stroke = Brushes.Red;
+                                    trail2.StrokeThickness = 15;
+                                    BlurEffect blur = new BlurEffect();
+                                    blur.Radius = 20;
+                                    trail2.Effect = blur;
+                                    trail = trail2;
+                                    canvas.Children.Add(trail);
+
+                                    
+                                }
+
+
+                                Canvas.SetLeft(brush, x - brush.Width / 2.0);
+                                Canvas.SetTop(brush, y - brush.Height);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
